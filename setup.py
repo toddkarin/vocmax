a3ee37d8a99d15f19a890ee36933dbf8b062283c
toddkarin <toddkarin@lbl.gov>
2019-10-02 11:44:38 -0700

    New version

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vocmax",
+    version="0.0.4",
+    author="toddkarin",
+    author_email="pvtools.lbl@gmail.com",
+    description="Calculate the maximum module open circuit voltage to find the maximum solar PV string length",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toddkarin/vocmax",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['numpy','pandas','pvlib','matplotlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
